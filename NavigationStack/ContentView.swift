824ff0a149293b6ea12a90596a507cfe1c73be2a
Preet Pambhar <preetpambhar73530@gmail.com>
2024-08-30 16:59:11 -0400

    Initial Commit

---
+++ NavigationStack/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by Preet Pambhar on 2024-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Preet Pambhar on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
