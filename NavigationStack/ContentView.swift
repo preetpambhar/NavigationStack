5f68e38ccca44fe20d16ba3a176ff1faf048a60c
Preet Pambhar <preetpambhar73530@gmail.com>
2024-08-31 21:34:57 -0400

    feat: Testing

--- NavigationStack/ContentView.swift
+++ NavigationStack/ContentView.swift
@@ -8,15 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct Series: Identifiable, Hashable {
+        let id = UUID()
+        var name: String
+        var rating: Double
+        
+    }
+    
+    struct Movies: Identifiable, Hashable{
+        let id = UUID()
+        var name: String
+    }
+    @State private var path  = NavigationPath()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        var tvSeries: [Series] = [Series( name: "Mirzapur 1", rating: 4.8),Series( name: "Mirzapur 2", rating: 4.6),Series( name: "Mirzapur 3", rating: 4.9),Series( name: "Mirzapur 4", rating: 4.9)]
+      
+        var movies: [Movies] = [
+        Movies(name: "Pushpa"),
+        Movies(name: "Titanic"),
+        Movies(name: "Siddat"),
+        Movies(name: "KGF")
+        ]
+        
+        NavigationStack(path: $path){
+            List{
+                Section("Series"){
+                    ForEach(tvSeries){ series in
+                       // Text(series.name)
+                        NavigationLink(series.name, value: series)
+                    }
+                }
+                Section("Movie"){
+                    ForEach(movies){movie in
+                        //Text(movies.name)
+                        NavigationLink(movie.name, value: movie)
+                    }
+                }
+            }.navigationTitle("Series")
+                .navigationDestination(for: Series.self){series in
+                    VStack{
+                        Text(series.name)
+                        Text("Rating:  \(series.rating, specifier:"%.1f")")
+                        Button("Add Move"){
+                            path.append(Movies(name: "Dhol"))
+                        }
+                        Button("GO Back"){
+                            path.removeLast()
+                        }
+                        Button("GO Root Home"){
+                            path = NavigationPath()
+                        }
+                }
+            } .navigationDestination(for: Movies.self){movies in
+                VStack{
+                    Text(movies.name)
+                    ForEach(tvSeries){ series in
+                       // Text(series.name)
+                        NavigationLink(series.name, value: series)
+                    }
+                        
+            }
         }
-        .padding()
+        }
+    }
+    @ViewBuilder
+    func TestView(message: String) -> some View{
+            VStack{
+                Text(message)
+            }
     }
 }
 
